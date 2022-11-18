92cd9eac2b2e69fc7e043335931dc503a1510c1e
-9Endom6% <Tommy.levasseur@icloud.com>
2022-11-18 13:39:39 -0500

    init

---
+++ Personal Projects/TicTacToe.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+    printf("     |     |     \n");
+    printf("     |     |     \n");
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("     |     |     \n");
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("     |     |     \n");
+
+    printf("     |     |     \n\n");
+}
